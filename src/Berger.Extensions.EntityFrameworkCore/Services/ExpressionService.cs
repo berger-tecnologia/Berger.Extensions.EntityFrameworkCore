4e41ee5c5fed0f0037e4f0005d596d70056520ca
Rafael Cronemberger <rafael@berger.com.br>
2023-07-29 17:58:01 -0300

    Initial commit

---
+++ src/Berger.Extensions.EntityFrameworkCore/Services/ExpressionService.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq.Expressions;
+
+namespace Berger.Extensions.EntityFrameworkCore
+{
+    public class ExpressionService<T> where T : class
+    {
+        #region Properties
+        private readonly List<string> _Elements = new List<string>();
+        private readonly List<Expression<Func<T, object>>> _Expressions = new List<Expression<Func<T, object>>>();
+        #endregion
+
+        #region Methods
+        public void Include(Expression<Func<T, object>> selector)
+        {
+            _Expressions.Add(selector);
+        }
+        public void Add(string selector)
+        {
+            _Elements.Add(selector);
+        }
+        public IEnumerable<Expression<Func<T, object>>> Expressions
+        {
+            get { return _Expressions; }
+        }
+        public IEnumerable<string> Elements
+        {
+            get { return _Elements; }
+        }
+        #endregion
+    }
+}
