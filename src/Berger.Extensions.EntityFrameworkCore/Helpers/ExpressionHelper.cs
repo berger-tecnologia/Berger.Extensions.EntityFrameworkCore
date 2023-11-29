192da2432b0cb8be029d3707e1b0287f11b23a22
Rafael Cronemberger <rafael@berger.com.br>
2023-11-28 22:33:06 -0300

    Package updates

--- src/Berger.Extensions.EntityFrameworkCore/Helpers/ExpressionHelper.cs
+++ src/Berger.Extensions.EntityFrameworkCore/Helpers/ExpressionHelper.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Berger.Extensions.Abstractions;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 
 namespace Berger.Extensions.EntityFrameworkCore
@@ -7,34 +6,6 @@ namespace Berger.Extensions.EntityFrameworkCore
     public static class ExpressionHelper
     {
         #region Methods
-        public static IQueryable<T> GetById<T>(this IQueryable<T> query, Guid id) where T : IBaseEntity<Guid>
-        {
-            return query.Where(e => e.Id == id);
-        }
-        public static IQueryable<T> GetByCode<T>(this IQueryable<T> query, string code) where T : ICoded
-        {
-            return query.Where(e => e.Code == code);
-        }        
-        public static IQueryable<T> GetVerified<T>(this IQueryable<T> query) where T : IVerified
-        {
-            return query.Where(e => e.Verified == true);
-        }        
-        public static IQueryable<T> GetByName<T>(this IQueryable<T> query, string name) where T : INamed
-        {
-            return query.Where(e => e.Name == name);
-        }
-        public static IQueryable<T> GetByEmail<T>(this IQueryable<T> query, string email) where T : IEmail
-        {
-            return query.Where(e => e.Email == email);
-        }
-        public static IQueryable<T> GetBySlug<T>(this IQueryable<T> query, string slug) where T : ISlug
-        {
-            return query.Where(e => e.Slug == slug);
-        }
-        public static IQueryable<T> GetActive<T>(this IQueryable<T> query) where T : IActivated
-        {
-            return query.Where(e => e.Active == true);
-        }
         public static IQueryable<T> ConfigureExpressions<T>(this IQueryable<T> query, ExpressionBaseService<T> fields) where T : class
         {
             var elements = fields.Elements;
