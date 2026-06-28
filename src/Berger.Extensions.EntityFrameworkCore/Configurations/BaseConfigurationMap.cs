096427de167dac0c94733f376f916eae14844bbb
Rafael Cronemberger <rafael@berger.com.br>
2026-06-28 20:21:15 -0300

    Package updates

--- src/Berger.Extensions.EntityFrameworkCore/Configurations/BaseConfigurationMap.cs
+++ src/Berger.Extensions.EntityFrameworkCore/Configurations/BaseConfigurationMap.cs
@@ -1,6 +1,4 @@
 using Microsoft.EntityFrameworkCore;
-;
-using Berger.Extensions.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
